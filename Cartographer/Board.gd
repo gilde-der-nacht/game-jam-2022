ed6292407804b99bd08cd86eef33784f59fa66f3
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-03-06 18:02:50 +0100

    Board: call game reset

--- Cartographer/Board.gd
+++ Cartographer/Board.gd
@@ -24,18 +24,22 @@ func _process(delta):
 		get_tree().quit()
 
 func _on_ButtonInstructionReset_pressed():
-	pass
+	var local_seed = 8 # randi()
+	$Instruction.game_reset(local_seed)
 
 func _on_ButtonInstructionNextTurn_pressed():
-	$Instruction.next_turn()
-	var season = $Instruction.get_season()
-	var edicts = $Instruction.get_edicts()
-	var explore = $Instruction.get_explore()
-	var last_turn = $Instruction.get_last_turn()
-	$Log.text += "Season: " + String(season) + "\n"
-	$Log.text += "Edicts: " + String(edicts) + "\n"
-	$Log.text += "Explore: " + String(explore) + "\n"
-	$Log.text += "LastTurn: " + String(last_turn) + "\n"
+	if $Instruction.get_last_turn() == "end":
+		$Log.text += "END!"
+	else:
+		$Instruction.next_turn()
+		var season = $Instruction.get_season()
+		var edicts = $Instruction.get_edicts()
+		var explore = $Instruction.get_explore()
+		var last_turn = $Instruction.get_last_turn()
+		$Log.text += "Season: " + String(season) + "\n"
+		$Log.text += "Edicts: " + String(edicts) + "\n"
+		$Log.text += "Explore: " + String(explore) + "\n"
+		$Log.text += "LastTurn: " + String(last_turn) + "\n"
 	$Log.cursor_set_line($Log.get_line_count()) # scroll to bottom
 
 func _on_ButtonMapReset_pressed():
