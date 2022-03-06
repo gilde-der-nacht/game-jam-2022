9c9ae8f992a6e6d569caf305cfe66d69299fa2e3
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-03-06 12:54:02 +0100

    add player name

--- Cartographer/Map/Map.gd
+++ Cartographer/Map/Map.gd
@@ -1,2 +1,14 @@
 extends Node2D
 
+var player_name = ""
+onready var player_name_output = get_node("PlayerName")
+
+func _ready():
+	update_player_name()
+
+func set_name(name):
+	player_name = name
+	update_player_name()
+
+func update_player_name():
+	player_name_output.set_text(player_name)
