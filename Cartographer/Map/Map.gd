7288612664d1ec4bd086d2a966cf12bc1a2587ce
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-03-06 18:27:55 +0100

    Map: Make current tile mirrorable

--- Cartographer/Map/Map.gd
+++ Cartographer/Map/Map.gd
@@ -10,12 +10,12 @@ const map_empty_state = [
 var map_state = map_empty_state.duplicate(true)
 onready var map_state_output = get_node("MapState")
 onready var grid_output = get_node("Grid")
-var current_tile_form = [Vector2(0,0), Vector2(0,1), Vector2(1,1)]
+var current_tile_form = [Vector2(0,0), Vector2(0,1), Vector2(1,1), Vector2(2,1)]
 var current_tile_kind = "WATER"
 onready var current_tile_grid_output = get_node("CurrentTileExample")
 onready var current_tile_output = get_node("CurrentTile")
 const TILE_SIZE = 50
-var mirrored = false
+var mirrored = true
 
 func _ready():
 	update_ui()
@@ -27,7 +27,7 @@ func set_player_name(name):
 func update_ui():
 	player_name_output.set_text("player_name: " + player_name)
 	map_state_output.set_text("map_state: " + String(map_state))
-	current_tile_output.set_text("current_tile: " + String(current_tile_form) + " | " + current_tile_kind)
+	current_tile_output.set_text("current_tile: " + String(current_tile_form) + " | " + current_tile_kind + " | mirrored: " + String(mirrored))
 	draw_map()
 	draw_current_tile(current_tile_form, current_tile_kind)
 
@@ -40,6 +40,7 @@ func get_map():
 	return map_state
 
 func draw_map():
+	delete_children(grid_output)
 	var tile = preload("res://Map/Tile.tscn")
 	var y_pos = 0
 	var x_pos = 0
@@ -82,16 +83,39 @@ func draw_tile(tile, state):
 			sprite.texture = empty_img
 
 func draw_current_tile(form: Array, type: String):
+	delete_children(current_tile_grid_output)
 	var tile = preload("res://Map/Tile.tscn")
 	for e in form:
 		var inst = tile.instance()
-		inst.position.x = e.x * TILE_SIZE
+		if mirrored:
+			var max_width = get_max_width(form)
+			inst.position.x = (max_width - e.x) * TILE_SIZE
+		else:
+			inst.position.x = e.x * TILE_SIZE
 		inst.position.y = e.y * TILE_SIZE
 		draw_tile(inst, type)
 		current_tile_grid_output.add_child(inst)
+
+func get_max_width(form: Array):
+	var max_width = 1
+	for e in form:
+		if e.x > max_width:
+			max_width = e.x
+	return max_width
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("mirror_tile"):
+		mirrored = !mirrored
+		update_ui()
 
 func on_Tile_mouse_entered(tile):
 	print("hover: " + String(tile.pos))
 
 func on_Tile_mouse_clicked(tile):
 	print("click: " + String(tile.pos))
+
+
+func delete_children(node):
+	for n in node.get_children():
+		node.remove_child(n)
+		n.queue_free()
