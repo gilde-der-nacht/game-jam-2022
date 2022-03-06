b70d65f8140659e9b67f5ce65ca25248123a7dad
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-03-06 12:58:34 +0100

    Board: set player names

---
+++ Cartographer/Board.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+func _ready():
+	pass
+
+func _on_ButtonInstructionReset_pressed():
+	pass
+
+func _on_ButtonInstructionNextTurn_pressed():
+	pass
+
+func _on_ButtonMapReset_pressed():
+	pass
+
+func _on_ButtonMapSetPlayerNames_pressed():
+	$Map1.set_name("Flutschi")
+	$Map2.set_name("Oliver")
+	$Map3.set_name("Thomas")
