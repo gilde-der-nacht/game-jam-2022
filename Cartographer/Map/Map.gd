2ecfa5c392e96694e5be4be2cf851cad519d5dbd
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-03-06 14:10:11 +0100

    Map: init map state

--- Cartographer/Map/Map.gd
+++ Cartographer/Map/Map.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 var player_name = ""
 onready var player_name_output = get_node("PlayerName")
+const map_empty_state = [
+	["EMPTY","EMPTY","EMPTY"],
+	["EMPTY","EMPTY","EMPTY"],
+	["EMPTY","EMPTY","EMPTY"],
+]
+var map_state = map_empty_state.duplicate(true)
+onready var map_state_output = get_node("MapState")
 
 func _ready():
 	update_ui()
@@ -12,6 +19,12 @@ func set_player_name(player_name):
 
 func update_ui():
 	player_name_output.set_text("player_name: " + player_name)
+	map_state_output.set_text("map_state: " + String(map_state))
 
 func reset_and_randomize(local_seed):
 	player_name = ""
+	map_state = map_empty_state.duplicate(true)
+	update_ui()
+
+func get_map():
+	return map_state
