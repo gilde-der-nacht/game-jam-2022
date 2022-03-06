36a9d349f07b162579f23d47b190daa0cc43a043
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-03-06 13:21:02 +0100

    Board: add camera

--- Cartographer/Board.gd
+++ Cartographer/Board.gd
@@ -3,6 +3,20 @@ extends Node2D
 func _ready():
 	pass
 
+func _process(delta):
+	var move_adjust = delta * 500.0
+	var camera = $Camera2D
+	if Input.is_action_pressed("ui_left"):
+		camera.position.x -= move_adjust
+	if Input.is_action_pressed("ui_right"):
+		camera.position.x += move_adjust
+	if Input.is_action_pressed("ui_up"):
+		camera.position.y -= move_adjust
+	if Input.is_action_pressed("ui_down"):
+		camera.position.y += move_adjust
+	if Input.is_action_just_released("ui_cancel"):
+		get_tree().quit()
+
 func _on_ButtonInstructionReset_pressed():
 	pass
 
@@ -16,3 +30,5 @@ func _on_ButtonMapSetPlayerNames_pressed():
 	$Map1.set_name("Flutschi")
 	$Map2.set_name("Oliver")
 	$Map3.set_name("Thomas")
+
+
